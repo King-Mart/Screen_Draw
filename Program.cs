a79c167144b11e562e32f75aefb324761c9e6685
KMart <ken.mart.d@gmail.com>
2024-11-09 03:55:16 -0500

    getting a window up

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+// Raylib_cs.Raylib.InitWindow(800, 600, "The grand evolution");
+
+int i = 0;
+while (i < 999_999_999) {
+    i += 1;
+
+}
+
+// Raylib_cs.Raylib.CloseWindow();
