6f0b19b2c60c183dfcc8a2eb348a9abb90b46ce9
KMart <ken.mart.d@gmail.com>
2024-11-10 00:27:03 -0500

    Added a square to the scene and some text!

--- Program.cs
+++ Program.cs
@@ -1,13 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using System.Numerics;
+using System.Reflection.Metadata;
 using System.Reflection.Metadata.Ecma335;
 using System.Runtime.InteropServices;
 using System.Security.Cryptography.X509Certificates;
+using Raylib_cs;
 
 class Program {
     
     static Vector2 villagerPosition;
+    static Vector2 villagerSize = new Vector2 (48,48);
+    static int currentPopulation;
 
     static int reallyLongLoop(int range) {
         int i = 0;
@@ -24,25 +28,40 @@ class Program {
 
     }
 
+    static int drawState() {
+        Raylib.BeginDrawing();
+
+        Raylib_cs.Raylib.ClearBackground(Raylib_cs.Color.RayWhite);
+        Raylib_cs.Raylib.DrawText("The current population is : " + currentPopulation.ToString(), 100, 100, 24, Raylib_cs.Color.DarkGray);
+        Raylib.DrawRectangleV(villagerPosition, villagerSize, Raylib_cs.Color.Purple);
+
+        Raylib.EndDrawing();
+        return 1;
+    }
+
     static void Main()
     {
         const int screenWidth = 800;
-        const int screenHeight = 8600;
+        const int screenHeight = 600;
 
         Console.WriteLine("Hello, World!");
         Raylib_cs.Raylib.InitWindow(screenWidth, screenHeight, "The grand evolution");
 
         villagerPosition = new Vector2((float)screenWidth / 2, (float)screenHeight / 2);
+        currentPopulation = 1;
 
         Raylib_cs.Raylib.SetTargetFPS(120);
 
         while (!Raylib_cs.Raylib.WindowShouldClose()) {
+            int updateStatus = updateState();
+            Console.WriteLine(format:"The frame computed with code " +  updateStatus.ToString());
+            int drawStatus = drawState();
+            Console.WriteLine(format:"The frame drew itself with code %d" + drawStatus.ToString());
             
         }
 
         
 
-        reallyLongLoop(999999999);
         Raylib_cs.Raylib.CloseWindow();
 
     }
