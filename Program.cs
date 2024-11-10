3ec4c6fe10a2547229cf23047df274a8489e0c62
KMart <ken.mart.d@gmail.com>
2024-11-09 23:24:13 -0500

    Starting to think outside of python

--- Program.cs
+++ Program.cs
@@ -1,12 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
 
-// Raylib_cs.Raylib.InitWindow(800, 600, "The grand evolution");
+class Program {
+    
+static int reallyLongLoop(int range) {
+    int i = 0;
+    while (i < range){
+        i += 1;
+    }
+    return 1;
+}
+    static void Main()
+    {
+        Console.WriteLine("Hello, World!");
+        Raylib_cs.Raylib.InitWindow(800, 600, "The grand evolution");
 
-int i = 0;
-while (i < 999_999_999) {
-    i += 1;
+        reallyLongLoop(999999999);
+        Raylib_cs.Raylib.CloseWindow();
+
+    }
+
 
 }
-
-// Raylib_cs.Raylib.CloseWindow();
