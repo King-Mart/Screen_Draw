d092bc120c34ca83a28224a8135b27ee22354a62
KMart <ken.mart.d@gmail.com>
2024-11-10 04:03:45 -0500

    Tested movement of the elements

--- Cell.cs
+++ Cell.cs
@@ -1,11 +1,17 @@
 using System.Numerics;
 
  class Cell {
-    public Vector2 position  = new Vector2 (0,0);
-    public Vector2 size = new Vector2 (1,1);
-    public Raylib_cs.Color color = Raylib_cs.Color.Black;
+    public Vector2 position;
+    public Vector2 size;
+    public Raylib_cs.Color color;
 
-    void draw() {
+    public Cell(Vector2 position, Vector2 size, Raylib_cs.Color color) {
+        this.position = position;
+        this.size = size;
+        this.color = color;
+    }
+
+    public void draw() {
         Raylib_cs.Raylib.DrawRectangleV(position, size, color);
     }
 
--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using System.Numerics;
+using System.Security.Cryptography;
 using Raylib_cs;
 
 class Program {
@@ -8,7 +9,10 @@ class Program {
     static Vector2 villagerPosition;
     static Vector2 villagerSize = new Vector2 (48,48);
     static int currentPopulation;
-    Cell testCell;
+    static Cell testCell;
+    static Vector2 villagerMove;
+    
+    static Random RandomNumGenerator = new Random();
 
     static int reallyLongLoop(int range) {
         int i = 0;
@@ -21,7 +25,10 @@ class Program {
     //Update the state of the simulation
     static int updateState() {
 
-        if (Raylib.IsKeyDown(Raylib_cs.KeyboardKey.Right)) villagerPosition.X += 2.0f;
+        villagerMove = new Vector2 ((float)RandomNumGenerator.Next(-1,2), (float)RandomNumGenerator.Next(-1, 2));
+
+        villagerPosition += villagerMove;
+        testCell.size += villagerMove;
         return 1;
 
     }
@@ -31,7 +38,9 @@ class Program {
 
         Raylib_cs.Raylib.ClearBackground(Raylib_cs.Color.RayWhite);
         Raylib_cs.Raylib.DrawText("The current population is : " + currentPopulation.ToString(), 100, 100, 24, Raylib_cs.Color.DarkGray);
+        Raylib_cs.Raylib.DrawText("The current movement vector is : " + villagerMove.ToString(), 130, 130, 24, Raylib_cs.Color.DarkGray);
         Raylib.DrawRectangleV(villagerPosition, villagerSize, Raylib_cs.Color.Purple);
+        testCell.draw();
 
         Raylib.EndDrawing();
         return 1;
@@ -41,17 +50,21 @@ class Program {
     {
         const int screenWidth = 800;
         const int screenHeight = 600;
+        
 
         Console.WriteLine("Hello, World!");
         Raylib_cs.Raylib.InitWindow(screenWidth, screenHeight, "The grand evolution");
 
         villagerPosition = new Vector2((float)screenWidth / 2, (float)screenHeight / 2);
         currentPopulation = 1;
+        testCell = new Cell(villagerPosition - villagerSize, new Vector2(16, 16), Raylib_cs.Color.Black);
 
         Raylib_cs.Raylib.SetTargetFPS(120);
 
         while (!Raylib_cs.Raylib.WindowShouldClose()) {
+            
             int updateStatus = updateState();
+
             // Console.WriteLine(format:"The frame computed with code " +  updateStatus.ToString());
             int drawStatus = drawState();
             // Console.WriteLine(format:"The frame drew itself with code %d" + drawStatus.ToString());
